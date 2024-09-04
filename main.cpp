de60d3bbbe6efa853d820254cc5af8e99074498d
Tucker Brown <tucobrow@ut.utm.edu>
2024-09-03 19:45:17 -0500

    adding to the feature branch

--- main.cpp
+++ main.cpp
@@ -6,12 +6,17 @@ int sum(int n) {
   return n + 1;
 }
 
+int product(int n) {
+  return n * 1;
+}
+
 int main() {
   int n;
   cout << "Enter a number. ";
   cin >> n;
 
   cout << sum(n) << endl;
+  cout << product(n) << endl;
   
   return 0;
 }
